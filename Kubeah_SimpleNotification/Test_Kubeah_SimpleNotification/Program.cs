08c0db87c04f042462843a7d0eea8b9c37224b5e
CrBast <31575093+CrBast@users.noreply.github.com>
2018-04-08 14:13:27 +0200

    Create solution

---
+++ Kubeah_SimpleNotification/Test_Kubeah_SimpleNotification/Program.cs
@@ -0,0 +1,19 @@
+﻿using Kubeah_SimpleNotification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Kubeah_SimpleNotification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KNotification kNotification = new KNotification();
+            Console.WriteLine("<< KNotification >> object initialize");
+            Console.Read();
+        }
+    }
+}
