b31ddf55f1138046851bf51812d6825b96bc39b8
Bastien <bastien.crettenand@hotmail.com>
2018-11-20 20:46:44 +0100

    Creating a functional version

---
+++ NuGet_Notification/Kubeah_SimpleNotification/DataTypes.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KNotifications
+{
+    /// <summary>
+    /// Return int for data types
+    /// </summary>
+    class DataTypes
+    {
+        private static Dictionary<int, string> data = new Dictionary<int, string>(){
+            {1, "title"}, {2, "content"}, {3, "opacity"}, {4, "backgroundColor"}, {5, "fontColor"}, {6, "time"}
+        };
+
+        public static string getName(int id){
+             return data[id].ToString();
+        }
+
+        /// <summary>
+        /// Get Title Id
+        /// </summary>
+        /// <returns>(int) Title Id</returns>
+        public static int Title() { return getId("title"); }
+
+        /// <summary>
+        /// Get Content Id
+        /// </summary>
+        /// <returns>(int) Content Id</returns>
+        public static int Content() { return getId("content"); }
+
+        /// <summary>
+        /// Get Opacity Id
+        /// </summary>
+        /// <returns>(int) Opacity Id</returns>
+        public static int Opacity() { return getId("opacity"); }
+
+        /// <summary>
+        /// Get BackgroundColor Id
+        /// </summary>
+        /// <returns>(int) BackgroundColor Id</returns>
+        public static int BackgroundColor() { return getId("backgroundColor"); }
+
+        /// <summary>
+        /// Get FontColor Id
+        /// </summary>
+        /// <returns>(int) FontColor Id</returns>
+        public static int FontColor() { return getId("fontColor"); }
+
+        /// <summary>
+        /// Get Time Id
+        /// </summary>
+        /// <returns>(int) Time Id</returns>
+        public static int Time() { return getId("time"); }
+
+        /// <summary>
+        /// Get dictionnary {(int) id, (string) definition}
+        /// </summary>
+        /// <returns>Dictionary<(int) id, (string) definition></returns>
+        public Dictionary<int, string> getDict()
+        {
+            return data;
+        }
+
+        private static int getId(string temp)
+        {
+            return Convert.ToInt32(data.FirstOrDefault(x => x.Value == temp));
+        }
+    }
+}
---
+++ NuGet_Notification/Kubeah_SimpleNotification/KNotification.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+
+namespace KNotifications
+{
+    class KNotification
+    {
+        // Example 1 ; true
+        public static Dictionary<int, string> data = new Dictionary<int, string>();
+        public void Show()
+        {
+            ShowApp();
+        }
+
+        public void setContent(string value)
+        {
+            AddOrOverwriteData(DataTypes.Content(), value);
+        }
+
+        public void setTitle(string value)
+        {
+            AddOrOverwriteData(DataTypes.Title(), value);
+        }
+
+        public void setOpacity(double value)
+        {
+            AddOrOverwriteData(DataTypes.Content(), DoubleWithComma(value));
+        }
+
+        public void setContent(string value)
+        {
+            AddOrOverwriteData(DataTypes.Content(), value);
+        }
+
+        // Static methods
+        public static void Show(string title, string content)
+        {
+            AddOrOverwriteData(DataTypes.Title(), title);
+            AddOrOverwriteData(DataTypes.Content(), content);
+        }
+
+        public static void Show(string title, string content, double opacity)
+        {
+            AddOrOverwriteData(DataTypes.Title(), title);
+            AddOrOverwriteData(DataTypes.Content(), content);
+            AddOrOverwriteData(DataTypes.Content(), DoubleWithComma(opacity));
+        }
+
+        
+
+        // PRIVATE
+        private static string ColorToStringHexa()
+        {
+            return "";
+        }
+
+        private static string DoubleWithComma(double value)
+        {
+            string result = value.ToString();
+            return result.Replace(".", ",");
+        }
+
+        /// <summary>
+        /// Use this for create and show notification
+        /// </summary>
+        private void ShowApp()
+        {
+            Directory.CreateDirectory("./App/notifications");
+            string title = DateTime.Now.ToString("dd.MM.yyyy_HH.mm.ss");
+            XmlWriter xmlWriter = XmlWriter.Create("./App/notifications" + title + ".xml");
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("param");
+            foreach (KeyValuePair<int, string> entry in data)
+            {
+                xmlWriter.WriteStartElement("param");
+                xmlWriter.WriteAttributeString("name", DataTypes.getName(entry.Key));
+                xmlWriter.WriteAttributeString("value", RemoveInvalidCharacters(entry.Value));
+                xmlWriter.WriteEndElement();
+            }
+            xmlWriter.WriteEndElement();
+            xmlWriter.Close();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("./App/notifications" + title + ".xml");
+            xmlDoc.Save("./App/notifications" + title + ".xml");
+
+
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var executablePath = $@"{currentDirectory}\App\Windows_Notification.exe";
+            var p = new Process{StartInfo = new ProcessStartInfo(executablePath)};
+            p.StartInfo.WorkingDirectory = Path.GetDirectoryName(executablePath);
+            p.Start();
+        }
+
+        private static void AddOrOverwriteData(int dataTypeId, string value)
+        {
+            if (data.ContainsKey(dataTypeId))
+                data[dataTypeId] = value;
+            else
+                data.Add(dataTypeId, value);
+        }
+
+        // https://sandeep-tada.blogspot.com/2014/02/c-systemargumentexception-hexadecimal_11.html
+        private static string RemoveInvalidCharacters(string str)
+        {
+            if (str == null) return null;
+
+            var formattedStr = new StringBuilder();
+
+            foreach (var ch in str)
+            {
+                if ((ch < 0x00FD && ch > 0x001F) || ch == '\t' || ch == '\n' || ch == '\r')
+                {
+                    formattedStr.Append(ch);
+                }
+            }
+            return formattedStr.ToString();
+        }
+    }
+}
